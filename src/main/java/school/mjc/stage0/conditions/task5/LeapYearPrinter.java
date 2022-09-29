b3c747232b1f52b7e3eda356306206385a232056
Bekhzod <Bekhzod1411@gmail.com>
2022-09-29 14:53:39 +0500

    new
    new

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean result = (year%4 ==0 && year %100 !=0) || year %400 ==0;
+        System.out.println(result ? "leap" : "not leap");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 2:
+                System.out.println(28);
+                break;
+            case 4,6,9,10,12:
+                System.out.println(31);
+                break;
+            default:
+                System.out.println("wrong number:");
+        };
     }
 }
